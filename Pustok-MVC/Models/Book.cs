1f2a8a55bcb9ab35f26c664292bd32c883917967
kamalrh <kamalrh@MOON09>
2024-04-23 18:51:09 +0400

    Add project files.

---
+++ Pustok-MVC/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Pustok_MVC.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public double CostPrice { get; set; }
+        public double SalePrice { get; set; }
+        public double DiscountPercent { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public Boolean StockStatus { get; set; }
+        public List<BookImage> BookImages { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+        public List<BookTag> BookTags { get; set; }
+    }
+}
---
+++ Pustok-MVC/Models/BookImage.cs
@@ -0,0 +1,11 @@
+﻿namespace Pustok_MVC.Models
+{
+    public class BookImage
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public Boolean PosterStatus { get; set; }
+        public Book Book { get; set; }
+    }
+}
